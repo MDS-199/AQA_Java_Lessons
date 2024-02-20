ca1db6149a03ea512530634d3636ea31548de0b5
MDS-199 <114391729+MDS-199@users.noreply.github.com>
2024-02-20 20:06:15 +0500

    3

--- maven/src/test/java/org/example/Pixel3aMobileTest.java
+++ maven/src/test/java/org/example/Pixel3aMobileTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Pixel3aMobileTest extends AndroidDriver
 {
     @Test
+    @Order(1)
+    @DisplayName("Test Sum")
     public void checkCalculateSum1and3Test() {
         initialize();
         driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Calculator\"]")).click();
@@ -17,6 +19,8 @@ public class Pixel3aMobileTest extends AndroidDriver
         assertEquals("4", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
     }
     @Test
+    @Order(2)
+    @DisplayName("Test Subtraction")
     public void checkCalculateSubtraction3and1Test() {
         initialize();
         driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
@@ -26,6 +30,8 @@ public class Pixel3aMobileTest extends AndroidDriver
         assertEquals("2", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
     }
     @Test
+    @Order(3)
+    @DisplayName("Test Multiplication")
     public void checkCalculateMultiplication6and2Test() {
         initialize();
         driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
@@ -35,6 +41,8 @@ public class Pixel3aMobileTest extends AndroidDriver
         assertEquals("6", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
     }
     @Test
+    @Order(4)
+    @DisplayName("Test Divide")
     public void checkCalculateDivide6and2Test() {
         initialize();
         driver.findElement(By.id("com.google.android.calculator:id/digit_6")).click();
